772d3aaea8877c48c37740503620bee6c8ec6522
poker <poker@DESKTOP-OO771KJ>
2023-10-23 11:37:03 +0200

    Commit con las clases Message y MessageType y la interfaz Signable creadas.

---
+++ Reto1_G5_Libraries/src/model/MessageType.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *This enum class is used to asign all the type of messages 
+ * @author Jessica
+ */
+public enum MessageType {
+    SIGNIN_REQUEST, 
+    SIGNUP_REQUEST, 
+    OK_RESPONSE, 
+    USER_ALREADY_EXISTS_RESPONSE, 
+    USER_NOT_FOUND_RESPONSE, 
+    PASSWORD_ERROR_RESPONSE, 
+    ERROR_RESPONSE;
+}
